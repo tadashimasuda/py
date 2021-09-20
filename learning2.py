459e4bcd29be9b901f41630e9f49d982e6e0b75b
tadashimasuda <tmasu4260@gmail.com>
2021-09-21 00:19:26 +0900

    リスト内包表記

--- learning2.py
+++ learning2.py
@@ -129,3 +129,17 @@ g = greeting()
 print(next(g))
 print(next(g))
 print(next(g))
+
+t = (1,2,3,4,5)
+r = []
+for i in t:
+    if i % 2 == 0:
+        r.append(i)
+print(r)
+
+r = [i for i in t if i % 2==0]
+print(r)
+
+
+
+
