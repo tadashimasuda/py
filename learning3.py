8257a77b33aeeed57ae965ae01dc189f8b6f8ee2
tadashimasuda <tmasu4260@gmail.com>
2021-09-24 23:13:38 +0900

    duck typing

--- learning3.py
+++ learning3.py
@@ -20,26 +20,68 @@ del person
 print('#####')
 
 class Car(object):
+    def __init__(self,model=None):
+        self.model = model
+        print('run __init__')
+
     def run(self):
         print('run')
+
+    def ride(self,person):
+        person.drive()
 
 class ToyotaCar(Car):
     pass
 
 class TeslaCar(Car):
+    def __init__(self,model='Model S',enable_auto_run=False):
+        super().__init__(model)
+        self._enable_auto_run = enable_auto_run
+
+    @property
+    def enable_auto_run(self):
+        return self._enable_auto_run
+
+    @enable_auto_run.setter
+    def enable_auto_run(self,is_enable):
+        self._enable_auto_run = is_enable
+
+    def run(self):
+        print('super run')
     def auto_run(self):
         print('auto run')
 
+# print('#####')
+# tesla_car = TeslaCar('model S')
+# tesla_car.enable_auto_run =True
+# print(tesla_car.enable_auto_run)
+
+class Person(object):
+    def __init__(self,age=1):
+        self.age = age
+
+    def drive(self):
+        if self.age >= 18:
+            print('ok')
+        else:
+            raise Exception('No drive')
+
+class Baby(Person):
+    def __init__(self,age =1):
+        if age < 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+class Adult(Person):
+    def __init__(self,age = 18):
+        if age >= 18:
+            super().__init__(age)
+        else:
+            raise ValueError
+
+baby = Baby()
+adult = Adult()
+
 car = Car()
-car.run()
-
-print('#####')
-
-toyota_car = ToyotaCar()
-toyota_car.run()
-
-print('#####')
-
-tesla_car = TeslaCar()
-tesla_car.run()
-tesla_car.auto_run()
+car.ride(adult)
